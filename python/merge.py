02240e0b5284bc5c9ff28291d91d037388dcac2b
Nathanaël Jourdane <roipoussiere@gmail.com>
2016-01-17 13:03:12 +0100

    bugfix on merge.py, and the script now makes a cleaned scad file before pushing it on GitHub.

--- python/merge.py
+++ python/merge.py
@@ -5,40 +5,56 @@
 # Date: december 30, 2015
 # GPL v2: http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html#SEC1
 
-# This script prepare the .scad file to be uploaded on Thingiverse:
-# - replace the parameters by the default parameters;
-# - replace the <include> declarations by their contents (Customizer app doesn't
-# supports multiple .scad files.);
-# - remove lines containing '/*TODO' or '/*NOTE'.
+# This script prepare the .scad file to be uploaded on Thingiverse and GitHub.
+# Create PUSHED_MAIN_FILE, by
+# - replacing the parameters by the default parameters;
+# - removing comments containing 'NOTE' or 'param=something'.
+# In addition for Thingiverse, create OUT_FILE, by
+# - replacing the <include> declarations by their contents (Customizer app
+# doesn't supports multiple .scad files.)
+
 
 from re import split
+from os.path import abspath, isfile
 
-MAIN_FILE = 'main.scad'
-ATTACHED_FILES = ['file2.txt', ...]
-OUT_FILE = 'customizable_digital_sundial.scad'
-PARAMETERS_FILE = 'default_parameters'
+SCAD_FOLDER = '.'
+MAIN_FILE = abspath(SCAD_FOLDER + '/' + 'sundial.scad')
+PUSHED_MAIN_FILE = abspath(SCAD_FOLDER + '/' + 'main.scad')
+OUT_FILE = abspath(SCAD_FOLDER + '/' + 'customizable_digital_sundial.scad')
+PARAMETERS_FILE = abspath('./python/default_parameters')
 
-# To set all parameters as default.
+for f in [PARAMETERS_FILE, OUT_FILE, MAIN_FILE, PUSHED_MAIN_FILE]:
+    if not isfile(f):
+        print('Error: %s doest\'n exist.' % f)
+
 parameters = {}
 with open(PARAMETERS_FILE) as f:
     for line in f:
         p = split(' = ', line.strip())
         parameters[p[0]] = p[1]
 
-with open(OUT_FILE, 'w') as out_f:
-    with open(MAIN_FILE) as main_f:
+with open(MAIN_FILE) as main_f:
+    with open(PUSHED_MAIN_FILE, 'w') as out_f:
         for line in main_f:
             if ' = ' in line:
-                p = split(r'( = |; )', line.strip())
+                if line[:4] == 'text':
+                    p = split(r'( = )', line.strip()[:-1]) + [';']
+                else:
+                    p = split(r'( = |; )', line.strip())
                 if p[0] in parameters:
-                    out_f.write(line.replace(p[2], parameters[p[0]]))
+                    out_f.write(line.replace(p[2], parameters[p[0]], 1))
                     continue
+            if line[:2] in ['/*','//']:
+                if any(s in line for s in ['TODO', 'NOTE', '=']):
+                    continue
+            out_f.write(line)
+
+with open(PUSHED_MAIN_FILE) as main_f:
+    with open(OUT_FILE, 'w') as out_f:
+        for line in main_f:
             if line[:9] == 'include <':
-                with open(line[9:-2]) as f:
+                with open(abspath(SCAD_FOLDER + '/' + line[9:-2])) as f:
                     for i, l in enumerate(f):
                         if i >= 9:
                             out_f.write(l)
-                continue
-            if '/*TODO' in line or '/*NOTE' in line:
-                continue
             out_f.write(line)
